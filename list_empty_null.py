4e159578cfab922dcc3260a4ad6019827959814d
Arthur Emig <arthur.erich.emig@gmail.com>
2023-05-08 11:22:24 +0200

    test empty list or None value

---
+++ list_empty_null.py
@@ -0,0 +1,20 @@
+list_empty = []
+
+list_non_empty = [1, 2]
+
+null_value = None
+
+if null_value:
+    print("Null value true")
+else:
+    print("Null value false")
+
+if list_empty:
+    print("list_empty true")
+else:
+    print("list_empty false")
+
+if list_non_empty:
+    print("list_non_empty true")
+else:
+    print("list_non_empty false")
